c816c10f7f42ae7f0ce0bad10dfc008186810f48
Artem Voronov <avoronov1808@gmail.com>
2020-10-16 18:23:34 +0300

    06 modified

--- 06/index.js
+++ 06/index.js
@@ -1,79 +1,75 @@
-const container = document.getElementById("container")
+const formContainer = document.getElementById("form-container")
 
+const Template = [`
+    <input placeholder="email" type="text" id="email">
+    <input placeholder="password" type="password" id="password">
+    <input type="submit" id="submit" value="Login">
+`,`
+    <input placeholder="login" type="text" id="login">    
+    <input placeholder="name" type="text" id="name">
+    <input placeholder="age" type="text" id="age">
+    <input placeholder="email" type="text" id="email">
+    <input placeholder="city" type="text" id="city">
+    <input placeholder="password" type="password" id="password">
+    <input placeholder="repeat password" type="password" id="repeat password">
+    <input type="submit" id="submit" value="Register">
+`]
+
+const errorMessage = {login:'Please check login', name: 'Name is empty',rPassword:"Password don't match"}
+
+let inputs
+let buttonSubmit
+
+
+const initForm = (template) => {
+    let form = document.createElement('form')
+    form.className = "form"
+    form.id = "form"
+    form.innerHTML = template
+    formContainer.append(form)
+    inputs = form.getElementsByTagName('input')
+    buttonSubmit = inputs.submit
+    buttonSubmit.disabled = check()
+    Array.from(inputs).forEach(elem => {
+        elem.addEventListener("click", () => {
+            listenButtons()
+            sendToCheckValues()
+        }, false);
+    })
+    // console.log(buttonSubmit)
+}
+initForm(Template[1])
+
+const removeForm = () => {
+    formContainer.innerHTML = ""
+    Array.from(inputs).forEach(elem => {
+
+    })
+}
 
 const switcher = document.getElementById("switcher")
-// const formDiv = document.getElementById("form-container")
-let form = document.getElementsByClassName("form")
-const alertDiv = document.getElementById("alert")
 
 
-const sections = switcher.getElementsByTagName("div")
-let active = sections[0].getElementsByClassName("active")
+const sections = switcher.getElementsByTagName('div')
+const forms = document.getElementsByTagName('form')
 
-// console.log(sections)
 
-const User = {
-    email: "email",
-    password: "password",
-    name:"PersonalName",
+function setActive() {
+    // sections.item(Math.abs(Array.from(sections).indexOf(event.target) - 1)).classList.remove('active')
+    // sections.item(Math.abs(Array.from(sections).indexOf(event.target))).classList.add('active')
+
+    // removeForm()
+    initForm(Template[Math.abs(Array.from(sections).indexOf(event.target))])
 }
 
-let text = [`Hello, ${User.name}`]
+// for (let section of sections) {
+//     section.addEventListener('click',setActive,false)
+// }
 
-if (active.innerHTML === "Login") {
-    form = form[0]
-    active = sections[0]
-    active.classList.add("active")
-    sections[1].classList.remove("active")
-} else {
-    form = form[1]
-    active = sections[1]
-    active.classList.add("active")
-    sections[0].classList.remove("active")
-}
-
-
-const inputs = Array.from(form.getElementsByTagName("input")).slice(0, 2)
-const button = document.getElementById("submit")
-
-
-// console.log(Array.from(sections).indexOf(active)) //index active class
-
-switcher.addEventListener('click', () => {
-    console.log(Array.from(event.target.classList))
-    if (Array.from(event.target.classList).indexOf("active") === -1) {
-        active = switcher.getElementsByClassName("active")[1]
-    }
-}, false)
-
-const check = () => {
-    if (inputs[0].value.length > 0 && inputs[1].value.length > 0) {
-        button.disabled = false
-    } else button.disabled = true
-}
-const sayHi = () => {
-    container.innerHTML = text[0]
-}
-const fault = () => {
-    // Не правильно ставить только на не верно введенное поле класс))
-    inputs[0].classList.add("invalid")
-    inputs[1].classList.add("invalid")
-    console.warn("so sad")
-    let alert = document.createElement("p")
-
-    alert.innerHTML = "email or password is wrong"
-    alert.className = "alert"
-    alertDiv.append(alert)
-    setTimeout(() => {
-        alert.remove()
-    },2000)
-}
-
-check()
 
 function listenButtons() {
     if (event.target.value !== undefined) {
-        check()
+        buttonSubmit.disabled = check()
         if (event.target.value.length > 0) {
             event.target.classList.remove("invalid")
         } else {
@@ -82,16 +78,68 @@ function listenButtons() {
     }
 }
 
-inputs[0].addEventListener("keyup", listenButtons, false);
-inputs[1].addEventListener("keyup", listenButtons, false);
+function check() {
+    let disabled = true
+    Array.from(inputs).forEach(input => {
+        if (input.type !== 'submit'){
+            if (input.value.length > 0) {
+                disabled = false
+            } else {
+                return disabled = true
+            }
+        }
+    })
+    return disabled
+}
 
-button.onclick = () => {
-    event.preventDefault()
-    const email = inputs[0].value
-    const password = inputs[1].value
-    if (email === User.email && password === User.password) {
-        sayHi()
-    } else {
-        fault()
+const fault = (errorMessage) => {
+    let alert = document.createElement("p")
+
+    alert.innerHTML = errorMessage
+    alert.className = "alert"
+    container.append(alert)
+
+    setTimeout(() => {
+        alert.remove()
+    },2000)
+}
+
+const sendToCheckValues = () => {
+    let userInfo = {}
+    Array.from(inputs).forEach(elem => {
+        if (elem.id !== 'submit' && elem.value.length > 0) {
+            userInfo[elem.id] = elem.value
+        }
+    })
+    console.log(userInfo)
+
+    let regExp = new RegExp("^[^,.]+$")
+
+    if (!regExp.test(userInfo.login)) {
+        fault(errorMessage.login)
+        inputs.login.classList.add("invalid")
     }
+
+    if (name.length === 0) {
+        fault(errorMessage.name)
+        inputs.name.classList.add("invalid")
+    }
+
+    console.log(`PASSORD: ${userInfo.password}`)
+    if (userInfo.password) {
+        if (userInfo.password.includes(userInfo["repeat password"], 0)) {
+            if (userInfo.password === userInfo["repeat password"]) {
+                console.log("NicePassword!")
+            } else {
+                fault(errorMessage.rPassword)
+                console.log(inputs["repeat password"])
+                inputs["repeat password"].classList.add("invalid")
+            }
+        }
+    }
+}
+
+submit.onclick = () => {
+    event.preventDefault()
+    // sendToCheckValues()
 }
